94ea0482485781651d732fc26ddbda45b767bfbd
Nicolaas <github@sunnysideup.co.nz>
2017-01-25 21:21:07 +1300

    PATCH: fixes

--- docs/en/example.php
+++ docs/en/example.php
@@ -371,11 +371,12 @@ $html = create_test();
 
             <div class="tfs-more-entries-always-show">
                 <span class="line">
-                    <strong>Currently Shown:</strong>
+                    <strong>Entries per Page:</strong>
                     <span class="total-showing-row-number">
                         <input type="number" min="1" max="10000" step="50" name="VisibleRowCount" class="visible-row-count" />
-                    </span>
-                    (<span class="min-row-number">0</span> - <span class="max-row-number">0</span>)
+                    </span> -
+                    <strong>Currently Shown Entries:</strong>
+                    <span class="min-row-number">0</span> - <span class="max-row-number">0</span>
                 </span>
             </div>
 
--- templates/Includes/TableFilterSortFooter.ss
+++ templates/Includes/TableFilterSortFooter.ss
@@ -13,3 +13,15 @@
         <span class="pagination"></span>
     </span>
 </div>
+
+
+<div class="tfs-more-entries-always-show">
+    <span class="line">
+        <strong>Entries per Page:</strong>
+        <span class="total-showing-row-number">
+            <input type="number" min="1" max="10000" step="50" name="VisibleRowCount" class="visible-row-count" />
+        </span> -
+        <strong>Currently Shown Entries:</strong>
+        <span class="min-row-number">0</span> - <span class="max-row-number">0</span>
+    </span>
+</div>
