dfdf716d5c8488cc14f08a74ded671a0d38731f7
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-03-07 11:55:33 +1300

    PATCH: typo

--- javascript/TableFilterSort.js
+++ javascript/TableFilterSort.js
@@ -1450,8 +1450,8 @@
                             url,
                             data,
                             function(returnedURL) {
-                                var width = Math.round(jQuery(window).width() * 0.97);
-                                var height = Math.round(jQuery(window).height() * 0.97);
+                                var width = Math.round(jQuery(window).width() * 0.93);
+                                var height = Math.round(jQuery(window).height() * 0.93);
                                 jQuery.modal(
                                     '<iframe src="'+returnedURL+'" width="'+width+'"height="'+height+'" style="border:0" id="tfs-pop-up-i-frame" name="tfs-pop-up-i-frame">',
                                     {
