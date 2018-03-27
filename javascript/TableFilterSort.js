57f37c518079fc841928a2046b7e1a6f208cf877
Nicolaas <github@sunnysideup.co.nz>
2018-03-28 07:25:30 +1300

    PATCH: fixes to common content show / no show

--- javascript/TableFilterSort.js
+++ javascript/TableFilterSort.js
@@ -1498,7 +1498,7 @@ jQuery(document).ready(
                         }
                     }
                 );
-                if(commonContentExists){
+                if(commonContentAdded){
                     var title = myob.myTableHolder.find('.'+ myob.commonContentHolderClass).attr("data-title");
                     if(typeof title !== "undefined") {
                         title = "<h3>"+title+"</h3>";
