7d78f22609f9c4054499ed40f34984e2c20b8afd
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-02-09 08:31:56 +1300

    PATCH: LINTER

--- client/javascript/doT.js
+++ client/javascript/doT.js
@@ -18,9 +18,9 @@
             defineParams:/^\s*([\w$]+):([\s\S]+)/,
             conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
             iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
-            varname:	"it",
-            strip:		true,
-            append:		true,
+            varname:    "it",
+            strip:        true,
+            append:        true,
             selfcontained: false,
             doNotSkipEncoded: false
         },
